2202609498d907d73c0070922e1f542315b758a1
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-04-09 10:35:21 +0200

    Update PmergeMe.cpp

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -11,16 +11,11 @@
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
-
-PmergeMe::PmergeMe() : _vecTime(0), _deqTime(0)
-{
-}
-
-PmergeMe::PmergeMe(const PmergeMe & other)
-{
-	*this = other;
-}
-
+ 
+PmergeMe::PmergeMe() : _vecTime(0), _deqTime(0) {}
+ 
+PmergeMe::PmergeMe(const PmergeMe & other) { *this = other; }
+ 
 PmergeMe & PmergeMe::operator=(const PmergeMe & other)
 {
 	if (this != &other)
@@ -32,181 +27,208 @@ PmergeMe & PmergeMe::operator=(const PmergeMe & other)
 	}
 	return *this;
 }
-
-PmergeMe::~PmergeMe()
-{
-}
-
+ 
+PmergeMe::~PmergeMe() {}
+ 
 double PmergeMe::getTime()
 {
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * 1000000.0) + tv.tv_usec;
+	return tv.tv_sec * 1000000.0 + tv.tv_usec;
 }
-
+ 
 bool PmergeMe::parseArguments(int argc, char **argv)
 {
 	for (int i = 1; i < argc; i++)
 	{
 		std::string s = argv[i];
 		if (s.empty() || s.find_first_not_of("0123456789") != std::string::npos)
-		{
 			return false;
-		}
 		long val = std::atol(s.c_str());
 		if (val <= 0 || val > std::numeric_limits<int>::max())
-		{
 			return false;
-		}
 		_vec.push_back(static_cast<int>(val));
 		_deq.push_back(static_cast<int>(val));
 	}
 	return true;
 }
-
-void PmergeMe::_binaryInsertVec(std::vector<int> & dest, int val)
+ 
+/* ========================== VECTOR ========================== */
+ 
+void PmergeMe::_sortVec(std::vector<int> & c)
 {
-	std::vector<int>::iterator it = std::lower_bound(dest.begin(), dest.end(), val);
-	dest.insert(it, val);
-}
-
-void PmergeMe::_mergeInsertionSortVec(std::vector<int> & c)
-{
-	size_t n = c.size();
-	if (n < 2)
+	if (c.size() < 2)
 		return;
-
+ 
+	// 1. Emparejar y separar en winners/pendants
 	std::vector<std::pair<int, int> > pairs;
-	for (size_t i = 0; i + 1 < n; i += 2)
+	for (size_t i = 0; i + 1 < c.size(); i += 2)
 	{
-		if (c[i] < c[i + 1])
-			pairs.push_back(std::make_pair(c[i + 1], c[i]));
-		else
+		if (c[i] > c[i + 1])
 			pairs.push_back(std::make_pair(c[i], c[i + 1]));
+		else
+			pairs.push_back(std::make_pair(c[i + 1], c[i]));
 	}
-
-	int stray = -1;
-	bool hasStray = (n % 2 != 0);
-	if (hasStray)
-		stray = c[n - 1];
-
+	bool hasStray = (c.size() % 2 != 0);
+	int stray = hasStray ? c.back() : -1;
+ 
+	// 2. Ordenar recursivamente los winners
 	std::vector<int> main_chain;
 	for (size_t i = 0; i < pairs.size(); i++)
 		main_chain.push_back(pairs[i].first);
-
-	_mergeInsertionSortVec(main_chain);
-
-	std::vector<int> pendants;
+	std::vector<std::pair<int, int> > savedPairs = pairs;
+	_sortVec(main_chain);
+ 
+	// 3. Reordenar pendants para mantener la relacion con main_chain
+	std::vector<int> pend;
+	std::vector<std::pair<int, int> > tmp = savedPairs;
 	for (size_t i = 0; i < main_chain.size(); i++)
 	{
-		for (size_t j = 0; j < pairs.size(); j++)
+		for (size_t j = 0; j < tmp.size(); j++)
 		{
-			if (main_chain[i] == pairs[j].first)
+			if (main_chain[i] == tmp[j].first)
 			{
-				pendants.push_back(pairs[j].second);
-				pairs.erase(pairs.begin() + j);
+				pend.push_back(tmp[j].second);
+				tmp.erase(tmp.begin() + j);
 				break;
 			}
 		}
 	}
-
-	if (!pendants.empty())
-		main_chain.insert(main_chain.begin(), pendants[0]);
-
+ 
+	// 4. Insertar pend[0] al inicio (es menor que main_chain[0])
+	if (!pend.empty())
+		main_chain.insert(main_chain.begin(), pend[0]);
+ 
+	// Rastrear posicion de cada winner a_k en main_chain
+	std::vector<size_t> pos(pend.size());
+	for (size_t i = 0; i < pend.size(); i++)
+		pos[i] = i + 1;
+ 
+	// 5. Secuencia de Jacobsthal para orden de insercion
 	std::vector<size_t> jacob;
 	jacob.push_back(1);
 	jacob.push_back(3);
-	while (jacob.back() < pendants.size())
+	while (jacob.back() < pend.size())
 		jacob.push_back(jacob[jacob.size() - 1] + 2 * jacob[jacob.size() - 2]);
-
+ 
+	// 6. Insertar pendants con busqueda binaria acotada
 	size_t last = 1;
 	for (size_t i = 1; i < jacob.size(); i++)
 	{
 		size_t curr = jacob[i];
-		if (curr > pendants.size())
-			curr = pendants.size();
+		if (curr > pend.size())
+			curr = pend.size();
 		for (size_t j = curr; j > last; j--)
-			_binaryInsertVec(main_chain, pendants[j - 1]);
+		{
+			size_t bound = pos[j - 1];
+			std::vector<int>::iterator it = std::lower_bound(
+				main_chain.begin(), main_chain.begin() + bound, pend[j - 1]);
+			size_t ins = it - main_chain.begin();
+			main_chain.insert(it, pend[j - 1]);
+			for (size_t k = 0; k < pos.size(); k++)
+			{
+				if (pos[k] >= ins)
+					pos[k]++;
+			}
+		}
 		last = curr;
 	}
-
+ 
+	// 7. Insertar stray si existe (sin cota, busqueda completa)
 	if (hasStray)
-		_binaryInsertVec(main_chain, stray);
+	{
+		std::vector<int>::iterator it = std::lower_bound(
+			main_chain.begin(), main_chain.end(), stray);
+		main_chain.insert(it, stray);
+	}
 	c = main_chain;
 }
-
-void PmergeMe::_binaryInsertDeq(std::deque<int> & dest, int val)
+ 
+/* ========================== DEQUE ========================== */
+ 
+void PmergeMe::_sortDeq(std::deque<int> & c)
 {
-	std::deque<int>::iterator it = std::lower_bound(dest.begin(), dest.end(), val);
-	dest.insert(it, val);
-}
-
-void PmergeMe::_mergeInsertionSortDeq(std::deque<int> & c)
-{
-	size_t n = c.size();
-	if (n < 2)
+	if (c.size() < 2)
 		return;
-
+ 
 	std::vector<std::pair<int, int> > pairs;
-	for (size_t i = 0; i + 1 < n; i += 2)
+	for (size_t i = 0; i + 1 < c.size(); i += 2)
 	{
-		if (c[i] < c[i + 1])
-			pairs.push_back(std::make_pair(c[i + 1], c[i]));
-		else
+		if (c[i] > c[i + 1])
 			pairs.push_back(std::make_pair(c[i], c[i + 1]));
+		else
+			pairs.push_back(std::make_pair(c[i + 1], c[i]));
 	}
-
-	int stray = -1;
-	bool hasStray = (n % 2 != 0);
-	if (hasStray)
-		stray = c[n - 1];
-
+	bool hasStray = (c.size() % 2 != 0);
+	int stray = hasStray ? c.back() : -1;
+ 
 	std::deque<int> main_chain;
 	for (size_t i = 0; i < pairs.size(); i++)
 		main_chain.push_back(pairs[i].first);
-
-	_mergeInsertionSortDeq(main_chain);
-
-	std::deque<int> pendants;
+	std::vector<std::pair<int, int> > savedPairs = pairs;
+	_sortDeq(main_chain);
+ 
+	std::deque<int> pend;
+	std::vector<std::pair<int, int> > tmp = savedPairs;
 	for (size_t i = 0; i < main_chain.size(); i++)
 	{
-		for (size_t j = 0; j < pairs.size(); j++)
+		for (size_t j = 0; j < tmp.size(); j++)
 		{
-			if (main_chain[i] == pairs[j].first)
+			if (main_chain[i] == tmp[j].first)
 			{
-				pendants.push_back(pairs[j].second);
-				pairs.erase(pairs.begin() + j);
+				pend.push_back(tmp[j].second);
+				tmp.erase(tmp.begin() + j);
 				break;
 			}
 		}
 	}
-
-	if (!pendants.empty())
-		main_chain.insert(main_chain.begin(), pendants[0]);
-
+ 
+	if (!pend.empty())
+		main_chain.insert(main_chain.begin(), pend[0]);
+ 
+	std::vector<size_t> pos(pend.size());
+	for (size_t i = 0; i < pend.size(); i++)
+		pos[i] = i + 1;
+ 
 	std::vector<size_t> jacob;
 	jacob.push_back(1);
 	jacob.push_back(3);
-	while (jacob.back() < pendants.size())
+	while (jacob.back() < pend.size())
 		jacob.push_back(jacob[jacob.size() - 1] + 2 * jacob[jacob.size() - 2]);
-
+ 
 	size_t last = 1;
 	for (size_t i = 1; i < jacob.size(); i++)
 	{
 		size_t curr = jacob[i];
-		if (curr > pendants.size())
-			curr = pendants.size();
+		if (curr > pend.size())
+			curr = pend.size();
 		for (size_t j = curr; j > last; j--)
-			_binaryInsertDeq(main_chain, pendants[j - 1]);
+		{
+			size_t bound = pos[j - 1];
+			std::deque<int>::iterator it = std::lower_bound(
+				main_chain.begin(), main_chain.begin() + bound, pend[j - 1]);
+			size_t ins = it - main_chain.begin();
+			main_chain.insert(it, pend[j - 1]);
+			for (size_t k = 0; k < pos.size(); k++)
+			{
+				if (pos[k] >= ins)
+					pos[k]++;
+			}
+		}
 		last = curr;
 	}
-
+ 
 	if (hasStray)
-		_binaryInsertDeq(main_chain, stray);
+	{
+		std::deque<int>::iterator it = std::lower_bound(
+			main_chain.begin(), main_chain.end(), stray);
+		main_chain.insert(it, stray);
+	}
 	c = main_chain;
 }
-
-
+ 
+/* ========================== RUN ========================== */
+ 
 void PmergeMe::run()
 {
 	std::cout << "Before: ";
@@ -221,15 +243,15 @@ void PmergeMe::run()
 		}
 	}
 	std::cout << std::endl;
-
-	double start_v = getTime();
-	_mergeInsertionSortVec(_vec);
-	_vecTime = getTime() - start_v;
-
-	double start_d = getTime();
-	_mergeInsertionSortDeq(_deq);
-	_deqTime = getTime() - start_d;
-
+ 
+	double t = getTime();
+	_sortVec(_vec);
+	_vecTime = getTime() - t;
+ 
+	t = getTime();
+	_sortDeq(_deq);
+	_deqTime = getTime() - t;
+ 
 	std::cout << "After:  ";
 	for (size_t i = 0; i < _vec.size(); i++)
 	{
@@ -242,7 +264,10 @@ void PmergeMe::run()
 		}
 	}
 	std::cout << std::endl;
-
-	std::cout << "Time to process " << _vec.size() << " elements with std::vector : " << _vecTime << " us" << std::endl;
-	std::cout << "Time to process " << _deq.size() << " elements with std::deque  : " << _deqTime << " us" << std::endl;
+ 
+	std::cout << "Time to process " << _vec.size()
+		<< " elements with std::vector : " << _vecTime << " us" << std::endl;
+	std::cout << "Time to process " << _deq.size()
+		<< " elements with std::deque  : " << _deqTime << " us" << std::endl;
 }
+ 
