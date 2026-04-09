c598c65504eceee69ba6a11256554d4723cb5cfe
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-04-09 10:33:56 +0200

    Update PmergeMe.hpp

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -12,41 +12,38 @@
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
-
+ 
 # include <iostream>
 # include <vector>
 # include <deque>
 # include <string>
 # include <algorithm>
-# include <iterator>
 # include <utility>
 # include <cstdlib>
 # include <limits>
 # include <sys/time.h>
-
+ 
 class PmergeMe
 {
 private:
-	std::vector<int> _vec;
-	std::deque<int> _deq;
-	double _vecTime;
-	double _deqTime;
-
-	void _mergeInsertionSortVec(std::vector<int> & c);
-	void _binaryInsertVec(std::vector<int> & dest, int val);
-	void _mergeInsertionSortDeq(std::deque<int> & c);
-	void _binaryInsertDeq(std::deque<int> & dest, int val);
-
-	static double getTime();
-
+	std::vector<int>	_vec;
+	std::deque<int>		_deq;
+	double				_vecTime;
+	double				_deqTime;
+ 
+	void	_sortVec(std::vector<int> & c);
+	void	_sortDeq(std::deque<int> & c);
+	static double	getTime();
+ 
 public:
 	PmergeMe();
 	PmergeMe(const PmergeMe & other);
 	PmergeMe & operator=(const PmergeMe & other);
 	~PmergeMe();
-
-	bool parseArguments(int argc, char **argv);
-	void run();
+ 
+	bool	parseArguments(int argc, char **argv);
+	void	run();
 };
-
+ 
 #endif
+ 
